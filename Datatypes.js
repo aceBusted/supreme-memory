43a7bf516dfdc6b78149fe310c4dd4d9c71a6c07
Aman Kumar Sah <73239818+raka334@users.noreply.github.com>
2023-01-12 23:08:29 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,7 @@
+// There are many different data types in Javascript.
+// There are mainly 7 types of data types.
+// String 
+// The text value is a string in JavaScript.
+// The Syntax Of String is ""
+// Operator in JavaScript 
+// 
