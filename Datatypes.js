251d92b1d8e3d60639d87989870ee2f759006561
Aman Kumar Sah <73239818+raka334@users.noreply.github.com>
2023-02-26 13:42:48 +0000

    s

--- Datatypes.js
+++
@@ -1,9 +0,0 @@
-// There are many different data types in Javascript.
-// There are mainly 7 types of data types.
-// String 
-// The text value is a string in JavaScript.
-// The Syntax Of String is ""
-// Operator in JavaScri
-console.log("This is my fisrt Code ")
-console.log("This code is for testing purpose ")
-
